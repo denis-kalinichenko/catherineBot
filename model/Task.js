3d0c6880981b896db3ccba6874ea14599d418fb0
Denis Kalinichenko <d.kalinichenkooo@gmail.com>
2018-01-05 16:21:38 +0100

    updated mongo, using ES6 classes

--- model/Task.js
+++ model/Task.js
@@ -2,7 +2,27 @@ const mongoose = {Schema} = require("./db");
 
 /* database schema for Task */
 const taskSchema = new Schema({
-    author: String,
+    chat_id: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        stripHtmlTags: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    }
 });
+
+class Task {
+
+}
+
+taskSchema.loadClass(Task);
 
 module.exports = mongoose.model("Task", taskSchema);
--- model/User.js
+++ model/User.js
@@ -24,16 +24,25 @@ const userSchema = new Schema({
     logs: [],
 });
 
-/* registering new user if doesn't exists, updating data and returning full info */
-userSchema.statics.check = function(chat, callback) {
-    const query = { chat_id: chat.id },
-        update = { username: chat.username },
-        options = {upsert: true, new: true, setDefaultsOnInsert: true};
+class User {
+    /**
+     * registering new user if doesn't exists, updating data and returning full info
+     *
+     * @param chat
+     * @param callback
+     */
+    static check(chat, callback) {
+        const query = { chat_id: chat.id };
+        const update = { username: chat.username };
+        const options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
-    this.findOneAndUpdate(query, update, options, (error, user) => {
-        if (error) return callback(error);
-        callback(null, user);
-    });
-};
+        this.findOneAndUpdate(query, update, options, (error, user) => {
+            if (error) return callback(error);
+            callback(null, user);
+        });
+    }
+}
+
+userSchema.loadClass(User);
 
 module.exports = mongoose.model("User", userSchema);
